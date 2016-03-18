1f1c2c162e6ce36f5cbe11c1b41be1ce70256679
Rudy <oleszczyk.m@gmail.com>
2016-03-18 16:54:52 +0100

    Doxygen again and again

--- MOPS_local_Broker/MQTT.h
+++ MOPS_local_Broker/MQTT.h
@@ -18,36 +18,79 @@
 //#include "MOPS.h"
 
 
+
+/**
+ * @struct FixedHeader
+ * @brief Structure containing fields to build
+ * standard 'fixed header' for MQTT.
+ *
+ * */
 typedef struct FixedHeader{
+	/** 4 bits of flags (1-retain, 2-QoS, 1-duplicate).*/
 	uint8_t Flags : 4;
+	/** MQTT message type. */
 	uint8_t MessageType : 4;
+	/** Message length (without fixed header) most significant byte. */
 	uint8_t RemainingLengthMSB;
+	/** Message length (without fixed header) least significant byte. */
 	uint8_t RemainingLengthLSB;
 }FixedHeader;
 
+/**
+ * @struct DummyStruct
+ * @brief Structure used for type casting into
+ * specific 2-byte 'variable header' in MQTT.
+ * */
 typedef struct DummyStruct{
+	/** Packet identifier most significant byte. */
 	uint8_t MSB_PacketIdentifier;
+	/** Packet identifier least significant byte. */
 	uint8_t LSB_PacketIdentifier;
 }DummyStruct;
 
 
 // **** Connection structures **** //
+/**
+ * @struct ProtocolName
+ * @brief Structures defines fields for
+ * 'protocol name' header in MQTT.
+ * */
 typedef struct ProtocolName{
+	/** Protocol name most significant byte. */
 	uint8_t MSB_Length; // = 0
+	/** Protocol name  least significant byte. */
 	uint8_t LSB_Length; // = 4
+	/** Protocol name in string form. */
 	uint8_t Name[4]; 	// = {'M', 'Q', 'T', 'T'};
 }ProtocolName;
 
+/**
+ * @struct ConnectVariableHeader
+ * @brief Defines specific 'variable header'
+ * used in 'Connection' message type in MQTT.
+ * */
 typedef struct ConnectVariableHeader{
+	/** Protocol name structure. */
 	ProtocolName PName;
+	/** Protocol level value. */
 	uint8_t ProtocolLevel; // = 4
+	/** Flags defines connection options. */
 	uint8_t ConnectFlags;
+	/** Keep Alive most significant byte. */
 	uint8_t MSB_KeepAlive;
+	/** Keep Alive least significant byte. */
 	uint8_t LSB_KeepAlive;
 }ConnectVariableHeader;
 
+/**
+ * @struct ConnACKVariableHeader
+ * @brief Defines specific 'variable header'
+ * used in 'Connection Acknowledgment' message type in MQTT.
+ * */
 typedef struct ConnACKVariableHeader{
+	/** Flags defines connection acknowledgment options. */
 	uint8_t ConnectACKFlags;
+	/** Connection acknowledgment return code. */
 	uint8_t ConnectReturnCode;
 }ConnACKVariableHeader;
 // **** Connection structures **** //
@@ -55,27 +98,74 @@ typedef struct ConnACKVariableHeader{
 
 
 // ***** Publish structures ***** //
+/**
+ * @struct TopicName
+ * @brief Structure contains fields
+ * to create 'Topic Name' object needed
+ * for publish MQTT message.
+ * */
 typedef struct TopicName{
+	/** Topic length most significant byte. */
 	uint8_t MSB_Length;
+	/** Topic length least significant byte. */
 	uint8_t LSB_Length;
+	/** Pointer to specified topic.*/
 	uint8_t *Topic;
 }TopicName;
 
+/**
+ * @struct PublishVariableHeader
+ * @brief Defines specific 'variable header'
+ * used in 'Publish' message type in MQTT.
+ * */
 typedef struct PublishVariableHeader{
+	/** 'Topic Name' structure containing topic length and pointer to it.*/
 	TopicName PName;
+	/** Packet identifier most significant byte. */
 	uint8_t MSB_PacketIdentifier;
+	/** Packet identifier least significant byte. */
 	uint8_t LSB_PacketIdentifier;
 }PublishVariableHeader;
 
+/**
+ * @typedef PubACKVariableHeader
+ * @brief Definition of 'variable header' for
+ * 'publish acknowledgment' message type in MQTT.
+ */
 typedef DummyStruct PubACKVariableHeader;
+/**
+ * @typedef PubCompVariableHeader
+ * @brief Definition of 'variable header' for
+ * 'publish complete' message type in MQTT.
+ */
 typedef DummyStruct PubCompVariableHeader;
+/**
+ * @typedef PubRelVariableHeader
+ * @brief Definition of 'variable header' for
+ * 'publish release' message type in MQTT.
+ */
 typedef DummyStruct PubRelVariableHeader;
+/**
+ * @typedef PubRecVariableHeader
+ * @brief Definition of 'variable header' for
+ * 'publish receive' message type in MQTT.
+ */
 typedef DummyStruct PubRecVariableHeader;
 // ***** Publish structures ***** //
 
 
 // **** Subscribe structures **** //
+/**
+ * @typedef SubscribeVariableHeader
+ * @brief Definition of 'variable header' for
+ * 'subscribe' message type in MQTT.
+ */
 typedef DummyStruct SubscribeVariableHeader;
+/**
+ * @typedef SubscribeVariableHeader
+ * @brief Definition of 'variable header' for
+ * 'unsubscribe' message type in MQTT.
+ */
 typedef DummyStruct UnSubscribeVariableHeader;
 // **** Subscribe structures **** //
 
