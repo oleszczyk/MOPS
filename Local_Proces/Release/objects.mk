db21d9ed07d1ea7997b88f2d83307fb29d6d2659
ReMeDi <remedi@xor.(none)>
2016-04-04 19:48:07 +0200

    adding orocos component use MOPS 2/2

--- Local_Proces/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,@/usr/xenomai/lib/posix.wrappers -L/usr/xenomai/lib -lpthread_rt -lxenomai -lpthread -lrt -lrtdm
-
